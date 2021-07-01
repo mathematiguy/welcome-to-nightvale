e20f1cc7b959b33098d36f72c21c72d95bf88d57
Caleb Moses <calebjdmoses@gmail.com>
2021-07-01 18:30:27 +1200

    update generate_text.py

--- scripts/generate_text.py
+++ scripts/generate_text.py
@@ -9,7 +9,7 @@ input_ids = tokenizer.encode(sentence, return_tensors='pt')
 output = model.generate(
     input_ids,
     do_sample=True,
-    max_length=2000,
+    max_length=1000,
     top_k=0,
     top_p=0.92,
     temperature=0.9)
